ae44659cbfe2a1de32496ef4b6b3b07c3f41fc84
Aaron Yates <aaronmyates@hotmail.com>
2020-07-30 20:37:34 -0400

    I just wanna get this done.

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      
+      <h1>My super cool MTG app</h1>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -5,9 +5,18 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import { applyMiddleware, createStore } from "redux";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
+import { cardReducer } from "./reducers/cardReducer";
+
+const store = createStore(cardReducer, applyMiddleware(thunk));
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
